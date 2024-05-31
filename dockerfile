3ae712029329718b7b449b20c0b71fea026381a6
Miguel Figueroa <miguelfigueroa90@gmail.com>
2024-05-31 06:39:32 -0400

    Testing connection to the database

--- dockerfile
+++ dockerfile
@@ -1,6 +1,9 @@
 FROM php:8.3.7-apache
 
 RUN apt update
-RUN docker-php-ext-install mysqli pdo pdo_mysql 
+RUN apt-get install -y libzip-dev zip
+RUN docker-php-ext-install mysqli pdo pdo_mysql zip
 RUN docker-php-ext-enable pdo_mysql
 RUN a2enmod rewrite
+
+COPY --from=composer:latest /usr/bin/composer /usr/local/bin/composer
--- public_html/index.php
+++ public_html/index.php
@@ -1,3 +1,19 @@
 <?php
 
-echo 'Hello world!';
+require 'vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$servername = $_ENV['db_database'];
+$username = $_ENV['db_user'];
+$password = $_ENV['db_password'];
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
