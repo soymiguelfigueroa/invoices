304142e613ca51b7b910a837119328670d629993
Miguel Figueroa <miguelfigueroa90@gmail.com>
2024-05-31 08:00:58 -0400

    Adding bootstrap

---
+++ public_html/src/bootstrap/app.php
@@ -0,0 +1,40 @@
+<?php
+
+// Loading .env content
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../../');
+$dotenv->load();
+
+// Adding debug functions
+if ($dotenv->required('APP_DEBUG')->isBoolean()) {
+    if (!function_exists('debug')) {
+        function debug($expression) {
+            if (filter_var($_ENV['APP_DEBUG'], FILTER_VALIDATE_BOOLEAN)) {
+                echo '<pre>';
+                var_dump($expression);
+                echo '</pre>';
+            }
+        }
+    }
+
+    if (!function_exists('dd')) {
+        function dd($expression) {
+            if (filter_var($_ENV['APP_DEBUG'], FILTER_VALIDATE_BOOLEAN)) {
+                debug($expression);
+                exit;
+            }
+        }
+    }
+}
+
+$servername = $_ENV['DB_DATABASE'];
+$username = $_ENV['DB_USER'];
+$password = $_ENV['DB_PASSWORD'];
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
