8a87e9b12f115e70e5063f865c34a7f9bb8fef46
Miguel Figueroa <miguelfigueroa90@gmail.com>
2024-06-03 13:25:00 -0400

    Adding twig

--- public_html/src/Controller/HomeController.php
+++ public_html/src/Controller/HomeController.php
@@ -6,12 +6,24 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 
 class HomeController
 {
+    private $twig;
+    
+    public function __construct()
+    {
+        $loader = new \Twig\Loader\FilesystemLoader('src/templates/');
+
+        $this->twig = new \Twig\Environment($loader);
+    }
+    
     public function index()
     {
-        $response = new JsonResponse();
-        $response->setData(array(
-            'data' => 'Hello'
-        ));
-        $response->send();
+        $this->render();
+    }
+
+    private function render()
+    {
+        echo $this->twig->render('home/index.twig');
+
+        return;
     }
 }
